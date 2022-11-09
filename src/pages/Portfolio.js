cd7a8f2ebed0925cb73b431e1410c69ffb78a34d
BrucelD14 <samuderaduta144@gmail.com>
2022-11-09 23:18:44 +0700

    add content portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,7 +7,37 @@ export default function Portfolio() {
 
   return (
     <section className="section">
-      <h1 className="section-title">PORTFOLIO PAGE</h1>
+      <h1 className="section-title">PORTFOLIO</h1>
+      <p className="section-description">
+        Ini adalah project dan sertifikat yang kumiliki:
+      </p>
+      <a href="https://newversion-brucel4movie.netlify.app/" target="_blank" className="portfolio" rel="noreferrer">
+        <div className="portfolio-card">
+          <h3 className="portfolio-title">brucel4movie</h3>
+          <p className="portfolio-description">Movie list app</p>
+          <br />
+          <span className="portfolio-stack">Javascript</span>
+          <span className="portfolio-stack">Bootstrap 5</span>
+        </div>
+      </a>
+      <a href="https://newversion-brucel4movie.netlify.app/" target="_blank" className="portfolio" rel="noreferrer">
+        <div className="portfolio-card">
+          <h3 className="portfolio-title">brucel4movie</h3>
+          <p className="portfolio-description">Movie list app</p>
+          <br />
+          <span className="portfolio-stack">Javascript</span>
+          <span className="portfolio-stack">Bootstrap 5</span>
+        </div>
+      </a>
+      <a href="https://newversion-brucel4movie.netlify.app/" target="_blank" className="portfolio" rel="noreferrer">
+        <div className="portfolio-card">
+          <h3 className="portfolio-title">brucel4movie</h3>
+          <p className="portfolio-description">Movie list app</p>
+          <br />
+          <span className="portfolio-stack">Javascript</span>
+          <span className="portfolio-stack">Bootstrap 5</span>
+        </div>
+      </a>
     </section>
   );
 }
