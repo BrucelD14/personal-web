8746bb0c21501c809df5f262abd80dc87fe71f08
BrucelD14 <samuderaduta144@gmail.com>
2022-11-10 16:37:24 +0700

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ export default function Footer() {
         <a href="mailto: samuderaduta144@gmail.com">KATAKAN HALO</a>
       </button>
       <div className="footer-icons">
-        <a href="https://github.com/BrucelD14">
+        <a href="https://github.com/BrucelD14" target="_blank" rel="noreferrer">
           <i className="ri-github-fill"></i>
         </a>
-        <a href="https://github.com/BrucelD14">
-          <i className="ri-github-fill"></i>
+        <a href="https://www.linkedin.com/in/brucel-duta-samudera-7155941b9/" target="_blank" rel="noreferrer">
+          <i class="ri-linkedin-fill"></i>
         </a>
-        <a href="https://github.com/BrucelD14">
-          <i className="ri-github-fill"></i>
+        <a href="https://instagram.com/_samudera" target="_blank" rel="noreferrer">
+        <i class="ri-instagram-fill"></i>
         </a>
       </div>
       <p className="footer-message">
