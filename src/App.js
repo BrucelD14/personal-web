f2845c21c4af7af261549e5f1831e314836a750d
BrucelD14 <samuderaduta144@gmail.com>
2022-11-10 16:24:18 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Blog from "./pages/Blog";
 import BlogDetail from "./pages/BlogDetail";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="app">
-    <div className="brand">MY PERSONAL WEB</div>
+      <div className="brand">MY PERSONAL WEB</div>
       <nav className="nav">
-        <Link to="/" className="nav-item">HOME</Link>
-        <Link to="/about" className="nav-item">ABOUT</Link>
-        <Link to="/portfolio" className="nav-item">PORTFOLIO</Link>
-        <Link to="/blog" className="nav-item">BLOG</Link>
+        <Link to="/" className="nav-item">
+          HOME
+        </Link>
+        <Link to="/about" className="nav-item">
+          ABOUT
+        </Link>
+        <Link to="/portfolio" className="nav-item">
+          PORTFOLIO
+        </Link>
+        <Link to="/blog" className="nav-item">
+          BLOG
+        </Link>
       </nav>
 
       <Routes>
@@ -24,6 +33,8 @@ function App() {
         <Route path="blog" element={<Blog />} />
         <Route path="blog/:id" element={<BlogDetail />} />
       </Routes>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <button className="footer-button">
+        <a href="mailto: samuderaduta144@gmail.com">KATAKAN HALO</a>
+      </button>
+      <div className="footer-icons">
+        <a href="https://github.com/BrucelD14">
+          <i className="ri-github-fill"></i>
+        </a>
+        <a href="https://github.com/BrucelD14">
+          <i className="ri-github-fill"></i>
+        </a>
+        <a href="https://github.com/BrucelD14">
+          <i className="ri-github-fill"></i>
+        </a>
+      </div>
+      <p className="footer-message">
+        &copy; 2022 Brucel D. Samudera. All Rights Reserved
+      </p>
+    </footer>
+  );
+}
