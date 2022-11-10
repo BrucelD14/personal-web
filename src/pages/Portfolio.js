71359b08ca9c0b84129c16e8f74b818377475ebd
BrucelD14 <samuderaduta144@gmail.com>
2022-11-10 12:45:24 +0700

    styling portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -20,22 +20,29 @@ export default function Portfolio() {
           <span className="portfolio-stack">Bootstrap 5</span>
         </div>
       </a>
-      <a href="https://newversion-brucel4movie.netlify.app/" target="_blank" className="portfolio" rel="noreferrer">
+      <a href="https://bmi-calculator-tpa2.netlify.app/" target="_blank" className="portfolio" rel="noreferrer">
         <div className="portfolio-card">
-          <h3 className="portfolio-title">brucel4movie</h3>
-          <p className="portfolio-description">Movie list app</p>
+          <h3 className="portfolio-title">BMI Calculator</h3>
+          <p className="portfolio-description">Calculator for your ideal weight & height</p>
           <br />
           <span className="portfolio-stack">Javascript</span>
           <span className="portfolio-stack">Bootstrap 5</span>
         </div>
       </a>
-      <a href="https://newversion-brucel4movie.netlify.app/" target="_blank" className="portfolio" rel="noreferrer">
+      <a href="dicoding.com/certificates/ERZR44O3WZYV" target="_blank" className="portfolio" rel="noreferrer">
         <div className="portfolio-card">
-          <h3 className="portfolio-title">brucel4movie</h3>
-          <p className="portfolio-description">Movie list app</p>
+          <h3 className="portfolio-title">Cloud Practitioner Essentials</h3>
+          <p className="portfolio-description">Belajar Dasar AWS Cloud</p>
           <br />
-          <span className="portfolio-stack">Javascript</span>
-          <span className="portfolio-stack">Bootstrap 5</span>
+          <span className="portfolio-stack">Dicoding Academy</span>
+        </div>
+      </a>
+      <a href="dicoding.com/certificates/ERZR44O3WZYV" target="_blank" className="portfolio" rel="noreferrer">
+        <div className="portfolio-card">
+          <h3 className="portfolio-title">UI/UX Design Mastery</h3>
+          <p className="portfolio-description">Learn UI/UX design at Skilvul</p>
+          <br />
+          <span className="portfolio-stack">Skilvul</span>
         </div>
       </a>
     </section>
