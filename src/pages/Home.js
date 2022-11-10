e39f84ca741dcf087ec8803ec3109adfda50e3aa
BrucelD14 <samuderaduta144@gmail.com>
2022-11-10 14:31:04 +0700

    change languages HOME

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,9 @@ export default function Home() {
 
   return (
     <section className="section home">
-      <h1 className="section-title">SELAMAT DATANG DI WEBSITE GUEH</h1>
+      <h1 className="section-title">WELCOME TO MY WEBSITE</h1>
       <p className="section-description">
-        Halo semua, ini personal web ku. Kamu bisa mendapatkan informasi
-        seputar pribadiku disini, salam kenal yaa. Have a nice day!
+        Hello there, it's my personal website. You can get personal information about me here, nice to meet you. Have a nice day!
       </p>
     </section>
   );
