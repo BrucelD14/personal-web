d72692db66a261604e0cf90e5744fea2d122ed10
BrucelD14 <samuderaduta144@gmail.com>
2022-11-10 11:06:05 +0700

    add content home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,10 +6,10 @@ export default function Home() {
   }, []);
 
   return (
-    <section className="section">
+    <section className="section home">
       <h1 className="section-title">SELAMAT DATANG DI WEBSITE GUEH</h1>
       <p className="section-description">
-        Halo semua, ini website baru aku. Kamu bisa mendapatkan informasi
+        Halo semua, ini personal web ku. Kamu bisa mendapatkan informasi
         seputar pribadiku disini, salam kenal yaa. Have a nice day!
       </p>
     </section>
