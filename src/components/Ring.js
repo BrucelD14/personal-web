853b2427ebdd62f6e6a1fa3f59832f1859067fbc
BrucelD14 <samuderaduta144@gmail.com>
2022-11-11 09:12:39 +0700

    add file Ring.js

---
+++ src/components/Ring.js
@@ -0,0 +1,10 @@
+export default function Ring() {
+  return (
+    <div className="ring-area">
+      <span className="ring"></span>
+      <span className="ring"></span>
+      <span className="ring"></span>
+      <span className="circle"></span>
+    </div>
+  );
+}
