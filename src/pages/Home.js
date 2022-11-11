ae50b96cdd740e91dac13e87e060578fc99d8ec9
BrucelD14 <samuderaduta144@gmail.com>
2022-11-11 10:10:58 +0700

    add home-image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,12 @@ export default function Home() {
   }, []);
 
   return (
-    <section className="section home">
+    <section className="section">
       <h1 className="section-title">WELCOME TO MY WEBSITE</h1>
       <p className="section-description">
         Hello there, it's my personal website. You can get personal information about me here, nice to meet you. Have a nice day!
       </p>
+      <img src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="home-image" className="home-image" />
     </section>
   );
 }
